83a82597c53257bdf80e413c119743f396f0b2c6
simonwandera <simonwandera12@gmail.com>
2022-12-12 02:01:11 +0300

    File display

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,12 @@
 
 from datetime import timedelta
-from flask import Flask, request, redirect, url_for, send_from_directory, jsonify, json
+from flask import Flask, request, redirect, url_for, send_from_directory, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_cors import CORS
+import os
+from werkzeug.utils import secure_filename
 from flask_jwt_extended import JWTManager
 
 
@@ -13,15 +15,20 @@ migrate = Migrate()
 
 def create_app(environment="dev"):
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     CORS(app, supports_credentials=True)
+
+    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['UPLOADED_PHOTOS_DEST'] = './uploads'
+    app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(APP_ROOT, "uploads")
     app.config['JWT_SECRET_KEY'] = 'Please remember to change me'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+
+    photos = UploadSet('photos', IMAGES)
+    configure_uploads(app, photos)
 
     jwt = JWTManager(app)
     db.init_app(app)
@@ -37,36 +44,27 @@ def create_app(environment="dev"):
         return app.send_static_file('index.html')
 
     
-    @app.route('/api/uploads', methods=['GET', 'POST'])
-    def upload_file():
-        print(request.files)
-        # for i in request.files:
-        #     print(i)
-        if request.method == 'POST' and 'photo' in request.files:
-            filename = photos.save(request.files['photo'])
-            print(filename)
-            
-            
-            # rec = Photo(filename=filename, user=g.user.id)
-            # rec.store()
-            return redirect(url_for('uploaded_file', filename=filename))
-        return '''
-        <!doctype html>
-        <title>Upload new File</title>
-        <h1>Upload new File</h1>
-        <form method=post enctype=multipart/form-data>
-        <input type=file name=photo>
-        <input type=submit value=Upload>
-        </form>
-        '''
+  
+
+    @app.route('/upload/<filename>', methods=["GET"])
+    def uploaded(filename):
+        file = secure_filename(filename)
+
+        print("SOmething here")
+        print(file)
+
+        download_image = send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],file)
+        print("\n\nWe are reporting from here \n")
+        return download_image
+
 
     @app.route('/api/uploads/<filename>')
     def uploaded_file(filename):
-        download_image = send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],
-                               filename)
+        print("Something here")
+       
+        # download_image = send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],filename)
 
-        print(app.config['UPLOADED_PHOTOS_DEST'])
-        return download_image
+        return send_file(app.config['UPLOADED_PHOTOS_DEST'] +"/"+ filename)
     
 
     from .rest.auth import auth as auth_blueprint
--- api/rest/productType.py
+++ api/rest/productType.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, request, json
+from flask import Blueprint, jsonify, request, send_from_directory
 from .. import db
 import jwt, os
 from api.controllers import productTypeController
 from werkzeug.utils import secure_filename
+
+from api import create_app
 
 
 productType = Blueprint('productType', __name__, url_prefix='/product_type')
@@ -10,20 +12,17 @@ productType = Blueprint('productType', __name__, url_prefix='/product_type')
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
+app = create_app()
+
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @productType.route('/upload', methods=["POST"])
-def login():
+def upload():
 
-    # print(request.json)
-
-    # if (request.json is None or len(request.json) == 0):
-    #     return {
-    #         "success": False,
-    #         "message": "No data provided"
-    #     }, 400
+    if not os.path.isdir(app.config['UPLOADED_PHOTOS_DEST']):
+        os.mkdir(app.config['UPLOADED_PHOTOS_DEST'])
 
     if 'file' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
@@ -39,11 +38,28 @@ def login():
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(productType.config['UPLOAD_FOLDER'], filename))
+        print(filename)
+        file.save(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename))
         resp = jsonify({'message': 'File successfully uploaded'})
-        resp.status_code = 201
+        resp.status_code = 200
         return resp
     else:
         resp = jsonify({'message': 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
         resp.status_code = 400
     return resp
+
+@productType.route('/upload/<filename>', methods=["GET"])
+def uploaded(filename):
+    file = secure_filename(filename)
+
+    print(filename)
+
+    imageList = os.listdir(app.config['UPLOADED_PHOTOS_DEST'])
+    print("\n\n")
+
+
+    print(imageList)
+
+    # download_image = send_from_directory(app.config['UPLOADED_PHOTOS_DEST'],file, as_attachment=True)
+    # print("\n\nWe are reporting from here \n")
+    return app.send_static_file(file)
